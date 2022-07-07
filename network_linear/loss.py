46785b16a71a9bfc59608bd153ac2d40169d74b3
fidabspd <fidabspd@gmail.com>
2022-07-07 13:57:42 +0900

    Add epsilon on input for log

--- network_linear/loss.py
+++ network_linear/loss.py
@@ -3,32 +3,34 @@ import torch
 
 class LogLikelihood(torch.nn.Module):
     
-    def __init__(self, scale=1.):
+    def __init__(self, scale=1., epsilon=1e-8):
         super().__init__()
         self.scale = scale
+        self.epsilon = epsilon
         
     def forward(self, x, x_hat):
-        log_likelihood = torch.mean(x * torch.log(x_hat) + (1 - x) * torch.log(1 - x_hat))
+        log_likelihood = torch.mean(x * torch.log(self.epsilon+x_hat) + (1 - x) * torch.log(self.epsilon+1-x_hat))
         return log_likelihood * self.scale
 
 
 class KLDivergence(torch.nn.Module):
 
-    def __init__(self, scale=1.):
+    def __init__(self, scale=1., epsilon=1e-8):
         super().__init__()
         self.scale = scale
+        self.epsilon = epsilon
 
     def forward(self, mu, sigma):
-        kl_divergence = 0.5 * torch.mean(torch.square(mu) + torch.square(sigma) - torch.log(1e-8 + torch.square(sigma)) - 1)
+        kl_divergence = 0.5 * torch.mean(torch.square(mu) + torch.square(sigma) - torch.log(self.epsilon+torch.square(sigma)) - 1)
         return kl_divergence * self.scale
 
 
 class ELBO(torch.nn.Module):
     
-    def __init__(self, scale=1.):
+    def __init__(self, scale=1., epsilon=1e-8):
         super().__init__()
-        self.log_likelihood = LogLikelihood(scale)
-        self.kl_divergence = KLDivergence(scale)
+        self.log_likelihood = LogLikelihood(scale, epsilon)
+        self.kl_divergence = KLDivergence(scale, epsilon)
         
     def forward(self, x, x_hat, mu, sigma):
         _log_likelihood = self.log_likelihood(x, x_hat)
@@ -42,7 +44,7 @@ class DiscriminatorLoss(torch.nn.Module):
     def __init__(self, scale=1.):
         super().__init__()
         self.scale = scale
-        self.bce_loss = torch.nn.BCELoss()
+        self.negative_bce_loss = torch.nn.BCELoss()
         
     def forward(self, real_disc_probs, fake_disc_probs):
         real_loss = self.bce_loss(real_disc_probs, torch.ones_like(real_disc_probs))
